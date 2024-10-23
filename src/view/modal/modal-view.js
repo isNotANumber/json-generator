36419a0023c4b6569aa4380b49229e8d1494fc0b
Kirill T <nan1911.work@gmail.com>
2024-10-23 13:56:47 +0500

    modal view added

---
+++ src/view/modal/modal-view.js
@@ -0,0 +1,33 @@
+import AbstractView from "../../framework/view/abstract-view";
+
+function createModalTemplate() {
+    return (
+        `
+        <div id="modal" class="modal">
+            <div class="modal__content">
+                <h2>Warning</h2>
+                <p id="modal-message">Are you sure you want to perform this action?</p>
+                <div class="modal__content__buttons">
+                    <button class="toolbar__button toolbar__button--clear">Yes</button>
+                    <button class="toolbar__button toolbar__button--apply">No</button>
+                </div>
+            </div>
+        </div>
+        `
+    );
+}
+
+/**
+ * Modal view class.
+ */
+export default class ModalView extends AbstractView {
+
+    /**
+     * Getter for modal template.
+     * @abstract
+     * @returns {string} Modal template as a string.
+     */
+    get template() {
+        return createModalTemplate();
+    }
+}
