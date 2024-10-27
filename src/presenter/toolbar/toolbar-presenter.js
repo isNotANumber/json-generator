44ce4e3cea418622f3e6f34705e6622a68510e9e
Kirill T <nan1911.work@gmail.com>
2024-10-27 19:48:32 +0500

    toolbar lesteners draft added

--- src/presenter/toolbar/toolbar-presenter.js
+++ src/presenter/toolbar/toolbar-presenter.js
@@ -1,16 +1,37 @@
-import { render } from "../../framework/render.js";
-import ToolbarView from "../../view/toolbar/toolbar-view.js"
+import { render } from '../../framework/render.js';
+import ToolbarView from '../../view/toolbar/toolbar-view.js';
 
 export default class ToolbarPresenter {
-    #container = null;
+  #container = null;
 
-    constructor({container}) {
-        this.#container = container;
-    }
+  constructor({ container }) {
+    this.#container = container;
+  }
 
-    init() {
-        const toolbar = new ToolbarView();
+  init() {
+    const toolbar = new ToolbarView({
+      onApplyClick: this.#handleApplyClick,
+      onClearClick: this.#handleClearClick,
+      onSaveClick: this.#handleSaveClick,
+      onCopyClick: this.#handleCopyClick,
+    });
 
-        render(toolbar, this.#container);
-    }
+    render(toolbar, this.#container);
+  }
+
+  #handleApplyClick = () => {
+    console.log('Apply clicked!');
+  };
+
+  #handleClearClick = () => {
+    console.log('Clear clicked!');
+  };
+
+  #handleSaveClick = () => {
+    console.log('Save clicked!');
+  };
+
+  #handleCopyClick = () => {
+    console.log('Copy clicked!');
+  };
 }
--- src/view/toolbar/toolbar-view.js
+++ src/view/toolbar/toolbar-view.js
@@ -1,16 +1,23 @@
-import AbstractView from "../../framework/view/abstract-view";
-import ButtonView from "../button/button-view.js";
+import AbstractView from '../../framework/view/abstract-view';
+import ButtonView from '../button/button-view.js';
 
 function createToolbarTemplate() {
-    const buttonsTemplate = [
-        new ButtonView({modifiers: ['button--green tlb-btn--apply'], buttonContent: 'Apply'}).template,
-        new ButtonView({modifiers: ['button--red tlb-btn--clear'], buttonContent: 'Clear'}).template,
-        new ButtonView({modifiers: ['tlb-btn--save'], buttonContent: 'Save'}).template,
-        new ButtonView({modifiers: ['tlb-btn--copy'], buttonContent: 'Copy'}).template,
-    ]
+  const buttonsTemplate = [
+    new ButtonView({
+      modifiers: ['button--green tlb-btn--apply'],
+      buttonContent: 'Apply',
+    }).template,
+    new ButtonView({
+      modifiers: ['button--red tlb-btn--clear'],
+      buttonContent: 'Clear',
+    }).template,
+    new ButtonView({ modifiers: ['tlb-btn--save'], buttonContent: 'Save' })
+      .template,
+    new ButtonView({ modifiers: ['tlb-btn--copy'], buttonContent: 'Copy' })
+      .template,
+  ];
 
-    return (
-        `
+  return `
         <div class="toolbar">
           <div class="toolbar__left">
             ${buttonsTemplate[0]}
@@ -21,28 +28,58 @@ function createToolbarTemplate() {
             ${buttonsTemplate[3]}
           </div>
         </div>
-        `
-    );
+        `;
 }
 
 /**
  * Toolbar view class.
  */
 export default class ToolbarView extends AbstractView {
-    // #onApplyClick = null;
-    // #onClearClick = null;
-    // #onSaveClick = null;
-    // #onCopyClick = null;
+  #handleApplyClick = null;
+  #handleClearClick = null;
+  #handleSaveClick = null;
+  #handleCopyClick = null;
 
-    // constructor({onApplyClick, onClearClick, onSaveClick, onCopyClick}) {
-    //     super();
-    // }
+  constructor({ onApplyClick, onClearClick, onSaveClick, onCopyClick }) {
+    super();
+    this.#handleApplyClick = onApplyClick;
+    this.#handleClearClick = onClearClick;
+    this.#handleSaveClick = onSaveClick;
+    this.#handleCopyClick = onCopyClick;
 
-    /**
-     * Getter for toolbar template.
-     * @returns {string} Toolbar template as a string.
-     */
-    get template() {
-        return createToolbarTemplate();
-    }
+    console.log(this.element)
+
+    this.element.querySelector('.tlb-btn--apply').addEventListener('click', this.#applyClickHandler);
+    this.element.querySelector('.tlb-btn--clear').addEventListener('click',this.#clearClickHandler);
+    this.element.querySelector('.tlb-btn--save').addEventListener('click',this.#saveClickHandler);
+    this.element.querySelector('.tlb-btn--copy').addEventListener('click',this.#copyClickHandler);
+  }
+
+  /**
+   * Getter for toolbar template.
+   * @returns {string} Toolbar template as a string.
+   */
+  get template() {
+    return createToolbarTemplate();
+  }
+
+  #applyClickHandler = (evt) => {
+    evt.preventDefault();
+    this.#handleApplyClick();
+  };
+
+  #clearClickHandler = (evt) => {
+    evt.preventDefault();
+    this.#handleClearClick();
+  };
+
+  #saveClickHandler = (evt) => {
+    evt.preventDefault();
+    this.#handleSaveClick();
+  };
+
+  #copyClickHandler = (evt) => {
+    evt.preventDefault();
+    this.#handleCopyClick();
+  };
 }
