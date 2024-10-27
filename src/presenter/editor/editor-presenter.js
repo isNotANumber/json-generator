dd9baf3b399d17eca2fa81be29bc85d4fd69faf1
Kirill T <nan1911.work@gmail.com>
2024-10-27 20:57:57 +0500

    editor presenter refactoring

--- src/presenter/editor/editor-presenter.js
+++ src/presenter/editor/editor-presenter.js
@@ -1,28 +1,58 @@
-import { render } from "../../framework/render";
-import EditorInputView from "../../view/editor/editor-input-view";
-import EditorOutputView from "../../view/editor/editor-output-view";
-import EditorView from "../../view/editor/editor-view";
-import KeyValueListView from "../../view/key-value-generator/key-value-list-view.js";
-import KeyValueGeneratorView from "../../view/key-value-generator/key-value-generator-view.js";
+import { render } from '../../framework/render';
+import EditorInputView from '../../view/editor/editor-input-view';
+import EditorOutputView from '../../view/editor/editor-output-view';
+import EditorView from '../../view/editor/editor-view';
+import KeyValueListView from '../../view/key-value-generator/key-value-list-view.js';
+import KeyValueGeneratorView from '../../view/key-value-generator/key-value-generator-view.js';
 
 export default class EditorPresenter {
-    #container = null;
+  #container = null;
 
-    constructor({container}) {
-        this.#container = container;
-    }
+  #editor = null;
+  #editorInput = null;
+  #editorOutput = null;
+  #generatorList = null;
+  #keyValueGeneratorView = null;
 
-    init() {
-        const editor = new EditorView();
-        const editorInput = new EditorInputView();
-        const editorOutput = new EditorOutputView();
-        const generatorList = new KeyValueListView();
-        const keyValueGeneratorView = new KeyValueGeneratorView();
+  constructor({ container }) {
+    this.#container = container;
+  }
 
-        render(editor, this.#container);
-        render(editorInput, editor.element);
-        render(editorOutput, editor.element);
-        render(generatorList, editorInput.element);
-        render(keyValueGeneratorView, generatorList.element);
-    }
+  init() {
+    this.#renderEditor(this.#container);
+    this.#renderEditorInput(this.#editor.element);
+    this.#renderEditorOutput(this.#editor.element);
+    this.#renderGeneratorList(this.#editorInput.element);
+    this.#renderKeyValueGenerator(this.#generatorList.element);
+  }
+
+  #renderEditor(container) {
+    this.#editor = new EditorView();
+
+    render(this.#editor, container);
+  }
+
+  #renderEditorInput(container) {
+    this.#editorInput = new EditorInputView();
+
+    render(this.#editorInput, container);
+  }
+
+  #renderEditorOutput(container) {
+    this.#editorOutput = new EditorOutputView();
+
+    render(this.#editorOutput, container);
+  }
+
+  #renderGeneratorList(container) {
+    this.#generatorList = new KeyValueListView();
+
+    render(this.#generatorList, container);
+  }
+
+  #renderKeyValueGenerator(container) {
+    this.#keyValueGeneratorView = new KeyValueGeneratorView();
+
+    render(this.#keyValueGeneratorView, container);
+  }
 }
