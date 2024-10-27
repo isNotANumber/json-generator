e7b705e094edb397e10694db5d2e11e78c437dd2
Kirill T <nan1911.work@gmail.com>
2024-10-27 18:44:45 +0500

    toolbar rework

--- src/presenter/toolbar/toolbar-presenter.js
+++ src/presenter/toolbar/toolbar-presenter.js
@@ -1,7 +1,5 @@
 import { render } from "../../framework/render.js";
-import ButtonView from "../../view/button/button-view.js";
 import ToolbarView from "../../view/toolbar/toolbar-view.js"
-import { TOOLBAR_DESCRIPTION } from "./toolbar-buttons-description.js";
 
 export default class ToolbarPresenter {
     #container = null;
@@ -14,21 +12,5 @@ export default class ToolbarPresenter {
         const toolbar = new ToolbarView();
 
         render(toolbar, this.#container);
-        this.#renderButtons(toolbar, TOOLBAR_DESCRIPTION);
-    }
-
-    #renderButton(button, container) {
-        render(button, container);
-    }
-
-    #renderButtons(toolbar, toolbarDescription) {
-        for (const side of Object.values(toolbarDescription)) {
-            const buttonsContainer = toolbar.element.querySelector(side.selector);
-
-            for (const button of Object.values(side.buttons)) {
-                const currentButton = new ButtonView({modifiers: button.modifiers, buttonContent: button.buttonContent});
-                this.#renderButton(currentButton, buttonsContainer);
-            }
-        }
     }
 }
--- src/view/toolbar/toolbar-view.js
+++ src/view/toolbar/toolbar-view.js
@@ -1,12 +1,24 @@
 import AbstractView from "../../framework/view/abstract-view";
+import ButtonView from "../button/button-view.js";
 
 function createToolbarTemplate() {
+    const buttonsTemplate = [
+        new ButtonView({modifiers: ['button--green tlb-btn--apply'], buttonContent: 'Apply'}).template,
+        new ButtonView({modifiers: ['button--red tlb-btn--clear'], buttonContent: 'Clear'}).template,
+        new ButtonView({modifiers: ['tlb-btn--save'], buttonContent: 'Save'}).template,
+        new ButtonView({modifiers: ['tlb-btn--copy'], buttonContent: 'Copy'}).template,
+    ]
+
     return (
         `
         <div class="toolbar">
           <div class="toolbar__left">
+            ${buttonsTemplate[0]}
+            ${buttonsTemplate[1]}
           </div>
           <div class="toolbar__right">
+            ${buttonsTemplate[2]}
+            ${buttonsTemplate[3]}
           </div>
         </div>
         `
@@ -17,6 +29,14 @@ function createToolbarTemplate() {
  * Toolbar view class.
  */
 export default class ToolbarView extends AbstractView {
+    // #onApplyClick = null;
+    // #onClearClick = null;
+    // #onSaveClick = null;
+    // #onCopyClick = null;
+
+    // constructor({onApplyClick, onClearClick, onSaveClick, onCopyClick}) {
+    //     super();
+    // }
 
     /**
      * Getter for toolbar template.
