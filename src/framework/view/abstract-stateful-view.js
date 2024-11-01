6fc98c4886558c2bf121fdeca410161331dc6f8a
Kirill T <nan1911.work@gmail.com>
2024-11-01 15:48:14 +0500

    abstact stateful class added

---
+++ src/framework/view/abstract-stateful-view.js
@@ -0,0 +1,52 @@
+import AbstractView from './abstract-view.js';
+
+/**
+ * Abstract view class with state
+ */
+export default class AbstractStatefulView extends AbstractView {
+  /** @type {Object} State object */
+  _state = {};
+
+  /**
+   * State update and rerender element method
+   * @param {Object} update Object with updated state
+   */
+  updateElement(update) {
+    if (!update) {
+      return;
+    }
+
+    this._setState(update);
+
+    this.#rerenderElement();
+  }
+
+  /**
+   * Restore listeners after rerender method
+   * @abstract
+   */
+  _restoreHandlers() {
+    throw new Error('Abstract method not implemented: restoreHandlers');
+  }
+
+  /**
+   * State update method
+   * @param {Object} update Object with updated state
+   */
+  _setState(update) {
+    this._state = structuredClone({...this._state, ...update});
+  }
+
+  /** Rerender element method */
+  #rerenderElement() {
+    const prevElement = this.element;
+    const parent = prevElement.parentElement;
+    this.removeElement();
+
+    const newElement = this.element;
+
+    parent.replaceChild(newElement, prevElement);
+
+    this._restoreHandlers();
+  }
+}
