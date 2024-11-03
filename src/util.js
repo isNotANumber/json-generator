9a3dcef97773830dfa19e7b82834db1e3398aa52
Kirill T <nan1911.work@gmail.com>
2024-11-03 23:38:09 +0500

    deleteElementById function added

--- src/util.js
+++ src/util.js
@@ -8,4 +8,27 @@ function generateRandomId(length = 10) {
     return result;
 }
 
-export { generateRandomId }
+function deleteElementById(obj, id) {
+    const newObj = { ...obj };
+    const idsToDelete = new Set();
+
+    function findChildren(parentId) {
+        for (const key in newObj) {
+            if (newObj[key].parentId === parentId) {
+                idsToDelete.add(key);
+                findChildren(key);
+            }
+        }
+    }
+
+    findChildren(id);
+    idsToDelete.add(id);
+
+    idsToDelete.forEach(id => {
+        delete newObj[id];
+    });
+
+    return newObj;
+}
+
+export { generateRandomId, deleteElementById }
