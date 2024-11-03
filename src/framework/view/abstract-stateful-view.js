718967f12e04e538253cba77e362d944d6202cda
Kirill T <nan1911.work@gmail.com>
2024-11-03 23:38:45 +0500

    updateElement method updated

--- src/framework/view/abstract-stateful-view.js
+++ src/framework/view/abstract-stateful-view.js
@@ -11,14 +11,18 @@ export default class AbstractStatefulView extends AbstractView {
    * State update and rerender element method
    * @param {Object} update Object with updated state
    */
-  updateElement(update) {
+  updateElement(update, modifier = 'update') {
     if (!update) {
       return;
     }
 
-    this._setState(update);
-
-    this.#rerenderElement();
+    if (modifier === 'update') {
+      this._setState(update);
+      this.#rerenderElement();
+    } else if (modifier === 'rewrite') {
+      this._state = update;
+      this.#rerenderElement();
+    }
   }
 
   /**
