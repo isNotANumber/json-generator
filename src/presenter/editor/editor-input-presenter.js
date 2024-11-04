c334216a1dd148755a91c8f6595df82c7da5e402
Kirill T <nan1911.work@gmail.com>
2024-11-04 11:38:21 +0500

    simplified editor input presenter

--- src/presenter/editor/editor-input-presenter.js
+++ src/presenter/editor/editor-input-presenter.js
@@ -1,4 +1,4 @@
-import { render, remove } from '../../framework/render.js';
+import { render } from '../../framework/render.js';
 import { generateRandomId, deleteElementById } from '../../util.js';
 import GeneratorItemView from '../../view/generator/generator-item-view.js';
 import GeneratorInputListView from '../../view/generator/generator-input-list-view.js';
@@ -10,8 +10,7 @@ export default class EditorInputPresenter {
 
   #generatorListModel = new GeneratorListModel();
 
-  #generatorInputListComponent = null;
-  #generatorItemComponent = null;
+  #generatorListComponent = null;
 
   constructor({ container }) {
     this.#container = container;
@@ -22,20 +21,19 @@ export default class EditorInputPresenter {
   }
 
   #renderGeneratorInputList(items, container) {
-    this.#generatorInputListComponent = new GeneratorInputListView({
+    this.#generatorListComponent = new GeneratorInputListView({
       items: items.map((item) => ({id: item.id, key: item.key, value: item.value})),
       onItemButtonClick: this.#handleGeneratorItemButtonClick,
     });
 
-    render(this.#generatorInputListComponent, container);
+    render(this.#generatorListComponent, container);
     this.#fillStateFromModel(this.#generatorListModel.generatorItems)
-    this.#renderListFromState(this.#generatorInputListComponent._state)
+    this.#renderListFromState(this.#generatorListComponent._state)
   }
 
   #renderGeneratorItem(item, container) {
-    this.#generatorItemComponent = new GeneratorItemView({id: item.id, key: item.key, value: item.value, parentId: item.parentId});
-
-    render(this.#generatorItemComponent, container);
+    const newItem = new GeneratorItemView({id: item.id, key: item.key, value: item.value, parentId: item.parentId});
+    render(newItem, container);
   }
 
   #handleGeneratorItemButtonClick = (evt) => {
@@ -47,16 +45,16 @@ export default class EditorInputPresenter {
         const newItem = {id: generateRandomId(), key: '', value: '', parentId: targetId};
   
         
-        this.#generatorInputListComponent.updateElement({[newItem.id]: newItem});
-        this.#renderListFromState(this.#generatorInputListComponent._state);
+        this.#generatorListComponent.updateElement({[newItem.id]: newItem});
+        this.#renderListFromState(this.#generatorListComponent._state);
   
       } else if (evt.target.classList.contains('gnrt-btn--remove')) {
         const targetId = item.dataset.id;
 
-        const newState = deleteElementById(this.#generatorInputListComponent._state, targetId);
+        const newState = deleteElementById(this.#generatorListComponent._state, targetId);
 
-        this.#generatorInputListComponent.updateElement(newState, 'rewrite');
-        this.#renderListFromState(this.#generatorInputListComponent._state);
+        this.#generatorListComponent.updateElement(newState, 'rewrite');
+        this.#renderListFromState(this.#generatorListComponent._state);
       }
     }
   };
@@ -67,10 +65,10 @@ export default class EditorInputPresenter {
       let currentGeneratorItemChildLocation = null;
 
       if (item.parentId === null) {
-        this.#renderGeneratorItem(currentGeneratorItem, this.#generatorInputListComponent.element);
+        this.#renderGeneratorItem(currentGeneratorItem, this.#generatorListComponent.element);
 
       } else {
-        currentGeneratorItemChildLocation = this.#generatorInputListComponent.element.querySelector(`[data-id='${item.parentId}'`).querySelector('.generator-input-list--nested');
+        currentGeneratorItemChildLocation = this.#generatorListComponent.element.querySelector(`[data-id='${item.parentId}'`).querySelector('.generator-input-list--nested');
         this.#renderGeneratorItem(currentGeneratorItem, currentGeneratorItemChildLocation);
       }
     }
@@ -98,7 +96,7 @@ export default class EditorInputPresenter {
         }
       }
 
-      this.#generatorInputListComponent._setState({[currentGeneratorItem.id]: currentGeneratorItem});
+      this.#generatorListComponent._setState({[currentGeneratorItem.id]: currentGeneratorItem});
     }
   }
 }
