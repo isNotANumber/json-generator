3a73d21c250e3f1576b05e5c0552cdbb64f9481c
Kirill T <nan1911.work@gmail.com>
2024-11-14 15:11:07 +0500

    sidebar refactoring

--- src/presenter/sidebar/sidebar-presenter.js
+++ src/presenter/sidebar/sidebar-presenter.js
@@ -1,7 +1,5 @@
 import { render, RenderPosition } from "../../framework/render.js";
 import SidebarView from "../../view/sidebar/sidebar-view.js";
-import SidebarElementView from "../../view/sidebar/sidebar-element-view.js";
-import { SIDEBAR_ELEMENTS } from "./sidebar-elements-description.js";
 
 export default class SidebarPresenter {
     #container = null;
@@ -14,18 +12,6 @@ export default class SidebarPresenter {
         const sidebar = new SidebarView();
 
         render(sidebar, this.#container, RenderPosition.AFTERBEGIN);
-        this.#renderSidebarElements(sidebar, SIDEBAR_ELEMENTS);
-    }
-
-    #renderSidebarElement(component, container) {
-        render(component, container);
-    }
-
-    #renderSidebarElements(sidebar, sidebarElementsDescription) {
-        const elementsContainer = sidebar.element.querySelector('.sidebar__menu');
-        for (const sidebarElement of Object.values(sidebarElementsDescription)) {
-            const currentElement = new SidebarElementView({sidebarElementText: sidebarElement.text})
-            this.#renderSidebarElement(currentElement, elementsContainer);
-        }
+        // this.#renderSidebarElements(sidebar, SIDEBAR_ELEMENTS);
     }
 }
--- src/view/sidebar/sidebar-view.js
+++ src/view/sidebar/sidebar-view.js
@@ -5,8 +5,20 @@ function createSidebarTemplate() {
         `
         <div class="sidebar">
             <div class="sidebar__title">Templates</div>
-            <ul class="sidebar__menu">
-            </ul>
+            <div class="sidebar__menu">
+            <div class="sidebar__item">
+                    <input type="radio" id="test0" name="template" value="test0">
+                    <label for="test0">Test0</label>
+                </div>
+                <div class="sidebar__item">
+                    <input type="radio" id="test1" name="template" value="test1">
+                    <label for="test1">Test1</label>
+                </div>
+                <div class="sidebar__item">
+                    <input type="radio" id="test2" name="template" value="test2">
+                    <label for="test2">Test2</label>
+                </div>
+            </div>
         </div>
         `
     );
